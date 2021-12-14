497a4888bca8007a17a6c589fbd437e71bd8669e
reesmcivor <reesmcivor@hotmail.com>
2021-12-14 16:20:55 +0000

    throw

--- inc/backstopBefore.js
+++ inc/backstopBefore.js
@@ -1,3 +1,7 @@
 module.exports = async function(page) {
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.56 Safari/537.36 TestingMaxpackVR');
+
+    page.on('error', function(err) {
+        throw err;
+    });
 };
